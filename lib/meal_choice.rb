c0106c476e48677d2cca96f4e19bb8abf3dec8d6
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 07:59:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,4 +2,5 @@
 def meal_choice(option1, option2, option3="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{option3} with #{option1} and #{option2}."
+  "A plate of #{option3} with #{option1} and #{option2}."
 end
