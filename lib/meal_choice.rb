d0480b615e563ceab01e21c9999d8ee8d3a71f65
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 07:58:02 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 # Your code here
-def meal_choice(option1, option2)
-  "A plate of meat with #{option1} and #{option2}."
+def meal_choice(option1, option2, option3="meat")
+  "A plate of #{option3} with #{option1} and #{option2}."
 end
